d69e401ad38749d0538b0914b885294efeae985e
Vitaliy Ponomarev <tourervit@gmail.com>
2021-07-28 11:12:59 +0300

    Add slugify

--- api/events/models/events.js
+++ api/events/models/events.js
@@ -1,8 +1,19 @@
-'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.name) {
+        data.slug = slugify(data.name, {lower: true});
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.name) {
+        data.slug = slugify(data.name, {lower: true});
+      }
+    },
+  },
+};
