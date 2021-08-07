3bd8bb5c4ae03cbd20a3208e7387ab6dd8270aa0
Vitaly <tourervit@gmail.com>
2021-08-07 17:03:32 +0300

    Add endpoint for user events

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,8 +1,20 @@
 'use strict';
-
+const { sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+	async me(ctx) {
+		const user = ctx.state.user
+		if( !user) {
+			return ctx.response.badRequest(null, [{message: [{id: "Not authorized - no authorization header"}]}])
+		}
+		const data = await strapi.services.events.find({user: user.id})
+		if (!data) {
+			return ctx.response.notFound()
+		}
+		return sanitizeEntity(data, {model: strapi.models.events})
+	}
+};
