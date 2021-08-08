b517376deb3c74b6368c4cc2d79c3253be00ae93
Vitaly <tourervit@gmail.com>
2021-08-08 12:11:46 +0300

    Add owner policy

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -1,5 +1,5 @@
 'use strict';
-const { sanitizeEntity } = require('strapi-utils');
+const { parseMultipartData ,sanitizeEntity } = require('strapi-utils');
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -16,5 +16,18 @@ module.exports = {
 			return ctx.response.notFound()
 		}
 		return sanitizeEntity(data, {model: strapi.models.events})
-	}
+	},
+
+	async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.events.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user;
+      entity = await strapi.services.events.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.events });
+  },
 };
